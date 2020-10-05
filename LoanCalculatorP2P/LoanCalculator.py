6f02c64b48d7ac7f8b4edcb7e2e72b892db1571e
Enrique Hidalgo <ehidalgopower@gmail.com>
2020-10-05 11:20:59 +0100

    package function creation - initialization git push origin master git status -

--- LoanCalculatorP2P/LoanCalculator.py
+++ LoanCalculatorP2P/LoanCalculator.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
 
-def borrow(amount, dflender):
+def borrow(amount):
     i = 0
+    x = amount
+    dflender = pd.read_csv("../DATA/market_example.csv", sep = ";")
+    dflender = dflender.sort_values(by="Rate").reset_index(drop=True).copy(deep=True)
     cost_borrowing = []
     while amount > 0:
-        if amount == dflender["cash_available"][i]:
-            cost_borrowing.append(amount * dflender["rate"][i])
-            amount -= dflender["cash_available"][i]
-        elif amount < dflender["cash_available"][i]:
-            cost_borrowing.append(amount * dflender["rate"][i])
-            amount -= dflender["cash_available"][i]
-        elif amount > dflender["cash_available"][i]:
-            cost_borrowing.append((amount-(amount-dflender["cash_available"][i])) * dflender["rate"][i])
-            amount -= dflender["cash_available"][i]
+        if amount == dflender["Available"][i]:
+            cost_borrowing.append(amount * dflender["Rate"][i])
+            amount -= dflender["Available"][i]
+        elif amount < dflender["Available"][i]:
+            cost_borrowing.append(amount * dflender["Rate"][i])
+            amount -= dflender["Available"][i]
+        elif amount > dflender["Available"][i]:
+            cost_borrowing.append((amount-(amount-dflender["Available"][i])) * dflender["Rate"][i])
+            amount -= dflender["Available"][i]
         i = i+1
 
-    return(np.sum(cost_borrowing))
+    interest = (np.sum(cost_borrowing))/x
+    returna = "the total cost of borrwing will be {}".format(np.sum(cost_borrowing))
+    returnb = "the total interest will be {}".format(interest)
+    return returna, returnb
