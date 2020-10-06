6775ed38bbbff8326b3c7ca70aeda8e2e1fc6090
Enrique Hidalgo <ehidalgopower@gmail.com>
2020-10-06 11:08:16 +0100

    Added function that updates DF

--- LoanCalculatorP2P/LoanCalculator.py
+++ LoanCalculatorP2P/LoanCalculator.py
@@ -1,34 +1,86 @@
 import pandas as pd
 import numpy as np
 
-def borrow(amount):
+def borrow(amount, update = True):
     i = 0
     initial_amount = amount
-    dflender = pd.read_csv("../DATA/market_example.csv", sep = ";")
+    dflender = pd.read_csv("../DATA/market_ex.csv", sep = ",")
     dflender = dflender.sort_values(by="Rate").reset_index(drop=True).copy(deep=True)
+    requested_amount = 0
+    number_borrowers = 0
+    ponderated_rate = 0
     cost_borrowing = []
     if (amount > 15000) or (amount < 100):
         return "Quantity not accepted"
-
     elif amount > dflender["Available"].sum():
-        return "insufficient funds"
-    else:
+        return "Insufficient funds"
+    elif update == False:
         while amount > 0:
             if amount == dflender["Available"][i]:
-                cost_borrowing.append(amount * dflender["Rate"][i])
+                requested_amount += dflender["Available"][i]
                 amount -= dflender["Available"][i]
-            elif amount < dflender["Available"][i]:
-                cost_borrowing.append(amount * dflender["Rate"][i])
-                amount -= dflender["Available"][i]
+                ponderated_rate += dflender["Rate"][i]*dflender["Available"][i]
+                number_borrowers +=1
             elif amount > dflender["Available"][i]:
-                cost_borrowing.append((amount-(amount-dflender["Available"][i])) * dflender["Rate"][i])
+                requested_amount += dflender["Available"][i]
                 amount -= dflender["Available"][i]
+                ponderated_rate += dflender["Rate"][i]*dflender["Available"][i]
+                number_borrowers +=1
+            elif amount < dflender["Available"][i]:
+                requested_amount += amount
+                ponderated_rate += dflender["Rate"][i] * amount
+                amount -= dflender["Available"][i]
+                number_borrowers +=1
             i = i+1
-    requested = initial_amount
-    interest = ((np.sum(cost_borrowing))/initial_amount)
-    returna = "Requested Amount : {}".format(requested)
-    returnb = "Rate:  {:.1f}%".format(interest*100)
-    returnc = "Monthly Repaiments: {:.2f}".format((interest*requested)/12)
-    returnd = "Total Repaiments: {}".format((requested* interest)+requested)
 
-    print(returna, returnb, returnc, returnd)
+        requested = requested_amount
+        interest = ponderated_rate/initial_amount
+        total_interest = requested+(requested*(interest*(((1+interest)**36))/(((1+interest)**36)-1)))
+        compound = 1 + interest
+        returna = "Requested Amount : {}".format(requested_amount)
+        returnb = "Rate:  {:.1f}%".format(interest*100)
+        returnc = "Monthly Repaiments: {:.2f}".format(((requested*(interest*(((1+interest)**36)))/(((1+interest)**36)-1))))
+        returnd = "Total Repaiments: {}".format((requested+(requested*(interest*(((1+interest)**36))/(((1+interest)**36)-1)))))
+
+        print(returna)
+        print(returnb)
+        print(returnc)
+        print(returnd)
+    #This section returns the information necessary for the borrower.
+    #Now we will update the dataframe so that the lenders are removed from the DF.
+    elif update == True:
+        while amount > 0:
+            if amount == dflender["Available"][i]:
+                requested_amount += dflender["Available"][i]
+                amount -= dflender["Available"][i]
+                ponderated_rate += dflender["Rate"][i]*dflender["Available"][i]
+                dflender["Available"][i] = np.nan
+            elif amount > dflender["Available"][i]:
+                requested_amount += dflender["Available"][i]
+                amount -= dflender["Available"][i]
+                ponderated_rate += dflender["Rate"][i]*dflender["Available"][i]
+                dflender["Available"][i] = np.nan
+            elif amount < dflender["Available"][i]:
+                requested_amount += amount
+                to_subtract = amount
+                ponderated_rate += dflender["Rate"][i] * amount
+                amount -= dflender["Available"][i]
+                dflender["Available"][i] = dflender["Available"][i] - to_subtract
+            i = i+1
+
+        requested = requested_amount
+        interest = ponderated_rate/initial_amount
+        total_interest = requested+(requested*(interest*(((1+interest)**36))/(((1+interest)**36)-1)))
+        compound = 1 + interest
+        dflender.dropna(inplace = True)
+        returna = "Requested Amount : {}".format(requested_amount)
+        returnb = "Rate:  {:.1f}%".format(interest*100)
+        returnc = "Monthly Repaiments: {:.2f}".format(((requested*(interest*(((1+interest)**36)))/(((1+interest)**36)-1))))
+        returnd = "Total Repaiments: {}".format((requested+(requested*(interest*(((1+interest)**36))/(((1+interest)**36)-1)))))
+        dflender.to_csv("../DATA/market_ex.csv")
+        print(returna)
+        print(returnb)
+        print(returnc)
+        print(returnd)
+
+        
